873bc922da470bebbcb89505222e01e899f35597
Daniel Zacarias <daniel.zacarias@gmail.com>
2008-04-03 09:35:34 +0000

    changed name of RecordedEmissionTest. It was outdated

--- web/test/unit/recorded_emission_test.rb
+++ web/test/unit/recorded_emission_test.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class AuthorEmissionTest < ActiveSupport::TestCase
+class RecordedEmissionTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   def test_truth
     assert true
