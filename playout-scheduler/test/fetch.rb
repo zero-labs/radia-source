dbcdcb912c547b7b9ee0f1fd16312ae74fcf4fef
ggb <madskaddie@gmail.com>
2010-01-17 16:00:17 +0000

    * fetch test uses now a canonical unit test

--- playout-scheduler/test/fetch.rb
+++ playout-scheduler/test/fetch.rb
@@ -3,21 +3,26 @@
 
 require 'rubygems'
 require 'eventmachine'
-
+require 'test/unit'
 
 dir = File.dirname(File.expand_path(__FILE__))
 
 require File.join(dir, '../config')
 require File.join(dir, '../playout_middleware')
+require File.join(dir, '../scheduler.rb')
 
-bcs = PlayoutMiddleware::fetch
-bc = nil
-bcs.each do |x| 
-    if x.attributes["type"] != "gap" then
-        bc = x
-        break
+class TestMiddleware < Test::Unit::TestCase
+
+    def test_fetch
+        PlayoutMiddleware::fetch.each do |bc| 
+            validate_broadcast bc
+        end
+    end
+
+    protected
+    def validate_broadcast bc
+            assert_instance_of(PlayoutMiddleware::Broadcast, bc)
+            assert(PlayoutMiddleware::Broadcast.is_valid_broadcast(bc))
     end
 end
-
-p bc
 
