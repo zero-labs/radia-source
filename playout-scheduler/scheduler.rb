35dac255edca5b81a903956c5aad07d4f6428a19
ggb <madskaddie@gmail.com>
2010-01-17 19:21:30 +0000

    * Broadcast Class:     - added "@type"     - added is_gap  * Mutex to protect Scheduler class  * to_play and is_gap?

--- playout-scheduler/scheduler.rb
+++ playout-scheduler/scheduler.rb
@@ -3,14 +3,15 @@ module PlayoutScheduler
    
     require 'rubygems'
     require 'eventmachine'
+    require 'thread'
 
     class Single
-        def initialize uri
-            p uri
+        def initialize uid
+            @uid = uid
         end
         
         def self.load_from_scheduler asset
-            p "I'm a single"
+            @uid = asset.id
         end
 
     end
@@ -26,7 +27,7 @@ module PlayoutScheduler
             if dtstart.eql?(dtend)
                 nil
             end
-            Broadcast.new("gap", dtstart, dtend, [Segment.new(:Playlist, "/playlists/tna.m3u", dtend-dtstart)])
+            Broadcast.new("gap", :gap, dtstart, dtend, [Segment.new(:Playlist, "/playlists/tna.m3u", dtend-dtstart)])
         end
 
         def self.new_gap_structure dtstart, dtend,*args
@@ -56,15 +57,19 @@ module PlayoutScheduler
             end
             PlayoutScheduler.const_get(type).send(:load_from_scheduler, asset)
         end
+
+        def to_play
+        end
     end
 
     class Broadcast
         attr_reader :name, :type, :dtstart, :dtend, :structure
         attr_accessor :timer
-        def initialize name, dtstart, dtend, structure = nil
+        def initialize name, type, dtstart, dtend, structure = nil
             @name = name
             @dtstart = dtstart
             @dtend = dtend
+            @type = type
             @structure = structure
             if @structure.nil?
                 @active_segment = nil
@@ -73,23 +78,42 @@ module PlayoutScheduler
             end
         end
 
-        def self.load_from_scheduler bc
-                if (!bc.respond_to?(:structure) or bc.structure.nil?) and
-                    (!bc.respond_to?(:bloc) or bc.bloc.nil?)
-                    struct = Gap.new_gap_structure(bc.dtstart, bc.dtend)
-                    Broadcast.new("gap", bc.dtstart, bc.dtend, struct)
-                elsif bc.respond_to?(:bloc)
-                    p bc
-                    struct = bc.bloc.segments.map do |segment|
-                        Segment.load_from_scheduler(segment)
-                    end
-                else
-                    struct = bc.structure.segments.map do |segment|
-                        Segment.load_from_scheduler(segment)
-                    end
-                end
-                Broadcast.new(bc.description, bc.dtstart, bc.dtend, struct)
+        # Returns a string with the asset to be played
+        def to_play
+            return "" if @active_segment.nil?
+            @active_segment.to_play 
         end
+
+        # Conversion method:
+        # - Receives: a PlayoutMiddleware::Broadcast
+        # - Returns: PlayoutScheduler::Broadcast
+        def self.load_from_scheduler bc
+            type = case bc.attributes["type"]
+                   when "emission" then :emission
+                   when "gap" then :gap
+                   #TODO else oops!
+                   end
+
+            if (!bc.respond_to?(:structure) or bc.structure.nil?) and
+                (!bc.respond_to?(:bloc) or bc.bloc.nil?)
+                return Gap.new_gap_broadcast(bc.dtstart, bc.dtend)
+            elsif bc.respond_to?(:bloc)
+                struct = bc.bloc.segments.map do |segment|
+                    Segment.load_from_scheduler(segment)
+                end
+            else
+                struct = bc.structure.segments.map do |segment|
+                    Segment.load_from_scheduler(segment)
+                end
+            end
+            name =  type==:emission ? bc.program_id : :gap
+            return Broadcast.new(name, type, bc.dtstart, bc.dtend, struct)
+        end
+
+        def is_gap?
+            return true if @type == :gap
+            return false
+        end 
 
         def to_s
             [@name,@dtstart,@dtend].join(", ")
@@ -100,14 +124,15 @@ module PlayoutScheduler
         attr_reader :broadcasts
         def initialize init, broadcasts = []
             @broadcasts = broadcasts
+            @global_lock = Mutex.new
             if init.key? :yaml then
                 @broadcasts = load_yaml init[:yaml]
                 @next_broadcast = get_next
-                current_broadcast()
+                rotate_broadcast()
             elsif init.key? :scheduler_uri
                 @broadcasts = load_from_scheduler 
                 @next_broadcast = get_next
-                current_broadcast()
+                rotate_broadcast()
             end
         end
 
@@ -119,31 +144,69 @@ module PlayoutScheduler
                 struct = broadcast["structure"].select do |segment|
                     Segment.new segment["type"].to_sym, segment["uri"], segment["length"]
                 end
-                broadcasts << Broadcast.new(broadcast["name"], broadcast["dtstart"], broadcast["dtend"], struct)
+                broadcasts << Broadcast.new(broadcast["name"], broadcast["type"], broadcast["dtstart"], broadcast["dtend"], struct)
             end
-            p broadcasts
             broadcasts
         end
 
         def load_from_scheduler
             require 'playout_middleware'
             PlayoutMiddleware::fetch.each do |broadcast|
+                p broadcast
                 broadcasts << Broadcast.load_from_scheduler(broadcast)
             end
-            p broadcasts
             broadcasts
         end
+
+
+        
+        # Updates the current and following broadcasts
+        # consuming 1 unit from the broadcast list top
+        def rotate_broadcast 
+            @global_lock.synchronize do 
+                @current_broadcast = @next_broadcast
+                p "Current broadcast: #{@current_broadcast}" if DEBUG
+                @next_broadcast = get_next @current_broadcast
+                if @current_broadcast.nil? then
+                    return
+                end
+                @current_broadcast.timer= EventMachine::Timer.new(
+                    @current_broadcast.dtend-Time.now) {rotate_broadcast()}
+            end
+        end
+
+
+        # Returns the following broadcast. It searches in the broadcasts list
+        # for the following, discarding any broadcast that lived in the past.
+        # 
+        # The broadcast list is updated so it must be enclosed by a lock. Isn't
+        # done because this method is called inside rotate_broadcast that
+        # already adquires the lock
+        #
         def get_next bc=nil
             now = bc.nil? ? Time.now : bc.dtend
             next_broadcast = nil
             while next_broadcast.nil?
                 if @broadcasts.empty? then
+                    # TODO: oops: the bc list is empty? Update is to slow to be done here
                     return nil
                 end
+
+                # If the following broadcast only starts in the future, a Gap is inserted
+                # or (if the following broadcast is already a Gap, it's merged)
                 if @broadcasts[0].dtstart > now then
-                     next_broadcast = Broadcast.new("gap", now, @broadcasts[0].dtstart)
-                     p "Next track: gap" if DEBUG
-                     break
+                    if @broadcasts[0].is_gap? 
+                        next_broadcast = Gap.new_gap_broadcast(now, @broadcasts[0].dtend)
+                        @broadcasts.shift()
+                    else
+                        next_broadcast = Gap.new_gap_broadcast(now, @broadcasts[0].dtstart)
+                    end
+                    p "Next track: gap" if DEBUG
+                    break
+
+                # If starded in the past either:
+                # - Returns if it hasn't finished or
+                # - Is discarded if has already finished
                 else
                     if @broadcasts[0].dtend > now
                         next_broadcast = @broadcasts.shift()
@@ -155,16 +218,6 @@ module PlayoutScheduler
                 end
             end
             next_broadcast
-        end
-        
-        def current_broadcast 
-            @current_broadcast = @next_broadcast
-            p "Current broadcast: #{@current_broadcast}" if DEBUG
-            @next_broadcast = get_next @current_broadcast
-            if @current_broadcast.nil? then
-                return
-            end
-            @current_broadcast.timer= EventMachine::Timer.new(@current_broadcast.dtend-Time.now) {current_broadcast()}
         end
     end
 end
