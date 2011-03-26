09768bb2417ea8ecaab503c001298aa30233cafe
Gil Brandao <madskaddie@gmail.com>
2011-03-26 15:46:47 +0000

    Massive commit: import process seems working
    A lot of tests are still needed but this serves as a good base for future work

--- scheduler/lib/jobs/schedule_download_and_merge_job.rb
+++ scheduler/lib/jobs/schedule_download_and_merge_job.rb
@@ -12,26 +12,46 @@ module Jobs
     end
 
     def perform
+      begin
+        log = Kernel::ScheduleUpdateLog.create! :dtstart => Time.now, :status => :initialized
+        program_schedule = RadiaSource::LightWeight::ProgramSchedule.instance
 
-      program_schedule = RadiaSource::LightWeight::ProgramSchedule.instance
+        log.status = :downloading
+        calendars = RadiaSource::LightWeight::ProgramSchedule.load_calendars Kernel::StructureTemplate.find(:all)
 
-      
+        rt = program_schedule.parse_calendars(calendars, @dtend)
 
-      calendars = RadiaSource::LightWeight::ProgramSchedule.load_calendars StructureTemplate.find(:all)
-      #TODO: break if calendars.has_key?(:errors)
+        if not rt[:ignored_repetitions].empty?
+          err = { :ignored_programs =>rt[:ignored_programs], :ignored_repetitions=> rt[:ignored_repetitions] }
+          log.operation_errors = err.to_yaml
+        end
 
-      program_schedule.prepare_update
 
-      #TODO: break if bc_hashes.has_key? "ignored_programs"
-      rt = program_schedule.parse_calendars(calendars, @dtend)
+        if not rt.has_key?(:originals) and not rt.has_key?(:repetitions)
+          keys = [:originals, :repetitions].select { |x| rt.has_key? x }
+          raise RadiaSource::LightWeight::ImportException.new("missing keys (#{__FILE__}:#{__LINE__}): " + keys.to_s )
+        end
 
-      if rt.has_key?(:originals) and rt.has_key?(:repetitions)
+        log.status = :parsing
+        program_schedule.prepare_update
         rt[:originals].each {|bc| program_schedule.add_broadcast! bc }
         rt[:repetitions].each {|bc| program_schedule.add_broadcast! bc }
-        program_schedule.save
-      else
-        $drt=rt
-        puts "uups", rt.class, rt.keys
+
+        log.status = :saving
+        if program_schedule.save
+          log.status = :completed
+        else
+          raise RadiaSource::LightWeight::ImportException.new("saving failed (#{__FILE__}:#{__LINE__})")
+        end
+      rescue RadiaSource::LightWeight::ImportException => e
+        log.status = :failed
+        log.operation_errors = e.to_operation_log_msg
+        puts e
+        puts e.backtrace
+        return false
+      ensure
+        log.dtend = Time.now;
+        log.save!
       end
     end
   end
