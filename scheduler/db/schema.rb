d8e1e3994b201e38635a57bf6b74db53f5f067c6
Daniel Zacarias <zacarias@radiozero.pt>
2011-03-31 21:10:46 +0100

    Removed merge error

--- scheduler/db/schema.rb
+++ scheduler/db/schema.rb
@@ -9,7 +9,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110221235323) do
 ActiveRecord::Schema.define(:version => 20110324195033) do
 
   create_table "asset_services", :force => true do |t|
